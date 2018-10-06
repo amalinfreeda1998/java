f1c84afafd6fb9b26f47123e2c1282fac3827b41
amalinfreeda1998 <43902561+amalinfreeda1998@users.noreply.github.com>
2018-10-06 14:07:03 +0530

    Add files via upload

--- postiveor.java
+++ postiveor.java
@@ -3,21 +3,21 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        int n;
-        Scanner s = new Scanner(System.in);
+        int b;
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter the number you want to check:");
-        n = s.nextInt();
+        b = scan.nextInt();
         if(n > 0)
         {
-            System.out.println("The given number "+n+" is Positive");
+            System.out.println("The given number "+b+" is Positive");
         }
-        else if(n < 0)
+        else if(b < 0)
         {
-            System.out.println("The given number "+n+" is Negative");
+            System.out.println("The given number "+b+" is Negative");
         }
         else
         {
-            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+            System.out.println("The given number "+b+" is neither Positive nor Negative ");
         }
     }
 }
